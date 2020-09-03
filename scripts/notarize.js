69cdb140b4f3dd09b65d74269424e5493ee74f57
astro019 <dmitry.yakov7@gmail.com>
2020-09-03 11:40:35 +0900

    remove notarize for mac

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -26,7 +26,7 @@ exports.default = async function notarizing (context) {
   const appName = context.packager.appInfo.productFilename
 
   return await notarize({
-    appBundleId: 'io.ark.desktop-wallet',
+    appBundleId: 'io.t69.desktop-wallet',
     appPath: `${appOutDir}/${appName}.app`,
     appleApiKey: process.env.APPLE_API_KEY_ID,
     appleApiIssuer: process.env.APPLE_API_KEY_ISSUER_ID
